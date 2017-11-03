00441dcc502c9f6d0b1471130844113597c5a4d9
Jordan Brown <ampzgunz@hotmail.com>
2017-11-03 05:24:08 -0400

    Update getTeams.js
    Fomatting

--- js/getTeams.js
+++ js/getTeams.js
@@ -1,92 +1,67 @@
 $(document).ready(function(){
-		var maxNumOfClans = 4;
-
-		function refreshList() {
-$('.current-teams').load('./teams.php');
-
-		}
-var refreshId = setInterval(refreshList, 1000);
-
-
-    $("#sendNew").submit(function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "index.php",
-            data: $(this).serialize(),
-            success: function(data) {
-                $('#teamTitle').val('');
-                // $('.current-teams').load('./teams.php');
-								var totalClans = ($('.team').length);
-
-								if (totalClans === maxNumOfClans){
-										alert("Max number of clans(" + maxNumOfClans + ") created.");
-								}
-            },
-            error: function() {
-                /* My Error */
-            }
-        });
-    });
-
-		$(document).on('click', '.join', function(){
-				var getID = $(this).attr("id");
-				$.post('./teams.php', { id: getID }, function(data) {
+	var maxNumOfClans = 4;
+	
+	function refreshList() {
+		$('.current-teams').load('./teams.php');
+		
+	}
+	var refreshId = setInterval(refreshList, 1000);
+	
+	$("#sendNew").submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "index.php",
+			data: $(this).serialize(),
+			success: function(data) {
+				$('#teamTitle').val('');
+				var totalClans = ($('.team').length);
+				
+				if (totalClans === maxNumOfClans){
+					alert("Max number of clans(" + maxNumOfClans + ") created.");
+				}
+			},
+			error: function() {
+			}
 		});
 	});
-
-	$(document).on('click', '.leaveTeam', function(){
-			var team = $(this).attr("id");
-			$.post('./teams.php', { leaveteam: team }, function(data) {
+	
+	$(document).on('click', '.join', function(){
+		var getID = $(this).attr("id");
+		$.post('./teams.php', { id: getID }, function(data) {
+		});
 	});
-
-});
-
-// 	$(document).on('click', '.leaveTeam', function(){
-// 			var teamID = $(this).attr("id");
-// 			$.post('./teams.php', { team: teamID }, function(data) {
-// 	});
-// });
-
-
-
-		function validate(){
-				$(".check").hide();
-				var newName = $('#guestName');
-
-				$(newName).on('keyup',function(){
-				if ($(this).val().length >= 3){
-					$(".check").css("top", "-2px");
-					$(".check").show().fadeIn(200);
-							$(".check").animate({
-									fontSize: "14px"
+	$(document).on('click', '.leaveTeam', function(){
+		var team = $(this).attr("id");
+		$.post('./teams.php', { leaveteam: team }, function(data) {
+		});
+		
+	});
+	
+	function validate(){
+		$(".check").hide();
+		var newName = $('#guestName');
+		
+		$(newName).on('keyup',function(){
+			if ($(this).val().length >= 3){
+				$(".check").css("top", "-2px");
+				$(".check").show().fadeIn(200);
+				$(".check").animate({
+					fontSize: "14px"
 				}, 200);
-
+				
 			} else if ($(this).val().length < 3){
-						$(".check").fadeOut(200);
-				}
-				});
-
-				$("#registerField").submit(function(e){
-						var newName = $('#guestName');
-						$.post('./teams.php', { user_page: newName.val() }, function(data) {
-						$('.current-teams').load('./teams.php');
-						});
-						$(".validateUser").hide();
-
-
-				});
-
+				$(".check").fadeOut(200);
+			}
+		});
+		
+		$("#registerField").submit(function(e){
+			var newName = $('#guestName');
+			$.post('./teams.php', { user_page: newName.val() }, function(data) {
+				$('.current-teams').load('./teams.php');
+			});
+			$(".validateUser").hide();
+		});
 	}
-validate();
-
-
-
-
-
-
-
-
-
-
+	validate();
 });
